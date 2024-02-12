165283ac1cc348358e6cdf993ae0cc51ebdac224
IlhamTahir <yantaq@bilig.biz>
2024-02-12 23:07:55 +0800

    lesson7: 请求对象

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Bind, Controller, Get, Req } from '@nestjs/common';
+import { Request } from 'express';
 @Controller('app')
 export class AppController {
   @Get('hello')
-  getHello(): string {
-    return '你好';
+  @Bind(Req())
+  getHello(request: Request) {
+    return request.headers;
   }
 }
